f3071f4d4734c02a6763fcc82c1cbcd70898d297
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-01-31 08:07:01 -0300

    commit

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -10,7 +10,7 @@ const ProjectContainer = () => {
 
                 <Project title='Celloluid' description='Rolls camera ecommerce website made with React' source='https://github.com/nahuel-retamoso/celluloid-retamoso' website='google.com' image='./ecommerce.png' />
 
-                <Project title='Movie Notes' description='A note app only for movies and series, register what you saw and make list' source='https://github.com/nahuel-retamoso/celluloid-retamoso' website='google.com' image='./ecommerce.png' />
+                <Project title='Movie Notes' description='A note app only for movies and series, register what you saw and make list' source='https://github.com/nahuel-retamoso/MovieNotes' website='https://movie-notes.vercel.app/signin' image='./movie-notes.png' />
 
                 <Project title='Ramen Lab' description='A restaurant website with booking system' source='https://github.com/nahuel-retamoso/ramen-lab' website='https://ramen-lab.vercel.app/' image='./ramen-lab.png' />
 
