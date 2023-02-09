b79813265c6f9a67a5169f92a1740a336a0afec7
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-02-09 00:34:02 -0300

    title height

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import {Text, Box } from "@chakra-ui/react";
 
 const Title = () => {
     return (
-            <Box mt={{base: '25%', '2xl': '5%'}} w={{base: '90%', '2xl': '44%'}} h={{base:'7vh', '2xl':'13.435vh'}}>
+            <Box mt={{base: '25%', '2xl': '5%'}} w={{base: '90%', '2xl': '44%'}} h={{base:'7vh', '2xl':'8vh'}}>
                     <Text fontFamily='monospace' fontSize={{base: '2em', '2xl': '4em'}} fontWeight='bold' color='whiteAlpha.900' mt='2%' align='center'>{'<'} <Text display='inline' color='whiteAlpha.900'>Nahuel Retamoso</Text> {'/ >'} </Text>
             </Box>
     );
