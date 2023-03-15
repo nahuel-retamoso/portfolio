c93b5fd8f984ba36c141c06dce0cccd0c63249b8
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-03-14 22:51:41 -0300

    styles

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import {Text, Box } from "@chakra-ui/react";
 const Title = () => {
     return (
             <Box mt={{base: '25%', 'xl': '5%'}} w={{base: '90%', 'xl': '44%'}} h={{base:'7vh', 'xl':'8vh'}}>
-                    <Text fontFamily='monospace' fontSize={{base: '2em', 'xl': '4em'}} fontWeight='bold' color='whiteAlpha.900' mt='2%' align='center'>{'<'} <Text display='inline' color='whiteAlpha.900'>Nahuel Retamoso</Text> {'/ >'} </Text>
+                    <Text fontFamily='monospace' fontSize={{base: '2em', 'xl': '3em'}} fontWeight='bold' color='whiteAlpha.900' mt='2%' align='center'>{'<'} <Text display='inline' color='whiteAlpha.900'>Nahuel Retamoso</Text> {'/ >'} </Text>
             </Box>
     );
 };
