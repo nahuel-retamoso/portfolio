59b9d86992c8360b55844d3d6ad28f8dfdbe082a
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-04-28 16:23:54 -0300

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { DownloadIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Button, Flex, Image, Link, Text, Icon, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { GoMarkGithub } from "react-icons/go";
 import { motion } from 'framer-motion';
@@ -14,28 +14,36 @@ const Navbar = () => {
     const highlightColor = useColorModeValue('custom.lightHighlight', 'custom.darkHighlight');
 
     return (
-       <Flex zIndex='overlay' backdropFilter='blur(10px)' pos='fixed' w='100%' h='7vh' align='center' >
-            <Flex ml={{base: '5%', 'xl': '20%'}} w={{base: '45%', 'xl': '25%'}} justify='center' align='center' >
-            <Text ml='3%' color={textColor} fontWeight='bold' fontSize={{base: '0.8em', 'xl': '1.5em'}} as='b' fontFamily='mono'>NR</Text>
-                <Text ml='3%' color={textColor} fontWeight='hairline' fontSize={{base: '0.8em', 'xl': '1.5em'}} as='b' fontFamily='mono'>Portafolio</Text>
+        <Flex zIndex='overlay' backdropFilter='blur(10px)' pos='fixed' w='100%' h='7vh' align='center'>
+            <Flex ml={{ base: '5%', 'xl': '0' }} w={{ base: '45%', 'xl': '50%' }} justify='left' align='center' pl='20%'>
+                <Text color={textColor} fontWeight='bold' fontSize={{ base: '0.8em', 'xl': '1.5em' }} as='b' fontFamily='mono'>NR</Text>
+                <Text ml='3%' color={textColor} fontWeight='hairline' fontSize={{ base: '0.8em', 'xl': '1.5em' }} as='b' fontFamily='mono'>Portafolio</Text>
+                <Link href="/mi-cv.pdf" download="Nahuel_Retamoso_CV">
+                <Button ml='40px'>
+                    <DownloadIcon mx='5px' />
+                    <Text pr='5px'>Descargar CV</Text>
+                </Button>
+                </Link>
             </Flex>
-            <Flex color={textColor} w='50%' justify='right' align='center' >
-                <Icon as={GoMarkGithub} mr='1%' w={6} h={6} />
-                <Link _hover={{textDecoration: 'none', bg:`${highlightColor}`}} p='0.5%' href='https://github.com/nahuel-retamoso/portfolio'>Source</Link>
+            <Flex w='50%' h='100%' justify='right' align='center' pr='20%'>
+                <Flex color={textColor} justify='right' align='center' >
+                    <Icon as={GoMarkGithub} marginX='7px' w={6} h={6} />
+                    <Link _hover={{ textDecoration: 'none', bg: `${highlightColor}` }} p='0.5%' href='https://github.com/nahuel-retamoso/portfolio'>Source</Link>
+                </Flex>
+                <MotionButton initial={false}
+                    animate={{ rotate: colorMode === 'light' ? 0 : 360 }}
+                    transition={{ duration: 0.5 }}
+                    bgColor="gray.400"
+                    ml="5%"
+                    mr={{ base: '5%', xl: '0' }}
+                    w="3%"
+                    h="70%"
+                    onClick={() => toggleColorMode()} >
+                    {colorMode == 'light' ? <MoonIcon /> : <SunIcon />}
+                </MotionButton>
             </Flex>
-            <MotionButton  initial={false} // Evita la animación inicial
-        animate={{ rotate: colorMode === 'light' ? 0 : 360}}
-        transition={{ duration: 0.5 }}
-        bgColor="gray.400"
-        ml="5%"
-        mr={{ base: '5%', xl: '20%' }}
-        w="3%"
-        h="70%"
-        onClick={() => toggleColorMode()} >
-                {colorMode == 'light' ? <MoonIcon /> : <SunIcon />}
-            </MotionButton>
-       </Flex>
+        </Flex>
     );
-    }
+}
 
 export default Navbar;
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -13,18 +13,18 @@ const ProjectContainer = () => {
     return (
         <Flex direction='column' mt='3%' w={{base:'90%', 'xl': '48%'}} h={{base: 'fit-content', 'xl': 'fit-content'}} align='center'>
             <Divider borderColor={textColor} />
-            <Text color='whiteAlpha.900' mt='45' mb='5%' p='0.5%' bgColor='blackAlpha.300' >Take a look at my work ㋡</Text>
+            <Text color='whiteAlpha.900' mt='45' mb='5%' p='0.5%' bgColor='blackAlpha.300' >Mira lo que he estado haciendo ㋡</Text>
             <Grid w='93%' h='100%' templateColumns={{base: 'repeat(1, 1fr)', 'xl': "repeat(2, 1fr)"}} gap={10}>
 
-                <Project title='Scroll to component' description='Scroll example in react' source='https://github.com/nahuel-retamoso/scroll-to-component' website='https://scroll-to-component.vercel.app/' image={scroll} />
+                <Project title='Scroll to component' description='Ejemplo de como hacer un scroll en react' source='https://github.com/nahuel-retamoso/scroll-to-component' website='https://scroll-to-component.vercel.app/' image={scroll} />
 
-                <Project title='Movie Notes' description='A notes app to save movies and series. You can organize your notes using tags.' source='https://github.com/nahuel-retamoso/MovieNotes' website='https://movie-notes.vercel.app/signin' image={movieNotes} />
+                <Project title='Movie Notes' description='Te permite crear notas y comentarios sobre películas y series, y agregar etiquetas para una fácil búsqueda en el futuro. ' source='https://github.com/nahuel-retamoso/MovieNotes' website='https://movie-notes.vercel.app/signin' image={movieNotes} />
 
-                <Project title='Ramen Lab' description='A restaurant website with reservation system' source='https://github.com/nahuel-retamoso/ramen-lab' website='https://ramen-lab.vercel.app/' image={ramenLab} />
+                <Project title='Ramen Lab' description='Website para un restaurante con sistema de reserva' source='https://github.com/nahuel-retamoso/ramen-lab' website='https://ramen-lab.vercel.app/' image={ramenLab} />
 
-                <Project title='Celluloid' description='Rolls camera ecommerce website made with React' source='https://github.com/nahuel-retamoso/celluloid-retamoso' website='https://celluloid-ecommerce.vercel.app' image={ecommerce} />
+                <Project title='Celluloid' description='Ecommerce hecho para el curso de React de Coderhouse' source='https://github.com/nahuel-retamoso/celluloid-retamoso' website='https://celluloid-ecommerce.vercel.app' image={ecommerce} />
 
-                <Project title='CNC website' description='CNC website build with Next Js, Tailwind and Framer Motion' source='https://github.com/nahuel-retamoso/CNCWebsite' website='https://cnc-website.vercel.app/' image={cncWebsite} />
+                <Project title='CNC website' description='Website hecho con NextJs, Tailwind y Framer Motion' source='https://github.com/nahuel-retamoso/CNCWebsite' website='https://cnc-website.vercel.app/' image={cncWebsite} />
                 
             </Grid>
         </Flex>
